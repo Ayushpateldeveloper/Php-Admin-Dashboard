89916215decf73e98c2cb053ff80261eb5d5072e
ayushpateldeveloper <ayushpateldeveloper@gmail.com>
2024-01-24 12:54:55 +0530

    day-7

--- includes/header.php
+++ includes/header.php
@@ -7,43 +7,38 @@
     <link rel="stylesheet" href="../includes/style.css">
     <script src="../includes/jquery.js"></script>
 
-    <!-- Include Bootstrap CSS once -->
+   <!-- Include Bootstrap CSS once -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
 
-<!-- Include jQuery and Bootstrap JS once -->
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Include Font Awesome for icons -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-<!-- Include DataTables CSS and JS once -->
+<!-- Include DataTables CSS and JS -->
 <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
 
-<!-- Include DataTables Buttons CSS and JS once -->
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
-<script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
-<!-- Include DataTables Buttons CSS and JS once -->
+<!-- Include DataTables Buttons CSS and JS -->
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
 <script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.colVis.min.js"></script>
+
 <!-- Include PDFMake and JSZip -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.68/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.68/vfs_fonts.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 
-<!-- Include DataTables Buttons CSS and JS once -->
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
-<script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.colVis.min.js"></script>
-
 <!-- boxicons script -->
 <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
 <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
-<!-- jQuery CDN -->
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<!-- Include jQuery and Bootstrap JS once -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+
+<!-- Your custom script -->
+<script src="../includes/jquery.js"></script>
+
 
 
 
--- pages/user.php
+++ pages/user.php
@@ -1,17 +1,24 @@
 <?php
-  ob_start();
-  include('../includes/header.php');
-  include('../includes/dbcon.php');
-  $sql = 'SELECT * FROM user_data WHERE isActive = 1';
-  $query = sqlsrv_query($Con, $sql);
+ob_start();
+include('../includes/header.php');
+include('../includes/dbcon.php');
+$sql = 'SELECT * FROM user_data WHERE isActive = 1';
+$query = sqlsrv_query($Con, $sql);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        #userDataTable_wrapper  .dt-buttons{
+            margin-bottom: 30px !important;
+        }
+    </style>
 </head>
+
 <body>
     <div class="container-fluid">
         <div class="row">
@@ -23,93 +30,94 @@
             </div>
         </div>
         <div>
-            <table class="table table-bordered table-hover" id="tableData">
+            <table class="table table-bordered table-hover mt-3" id="userDataTable">
                 <thead>
-                    <tr>
-                        <th scope="col">Id</th>
-                        <th scope="col">Profile Photo</th>
-                        <th scope="col">Emp ID</th>
-                        <th scope="col">First Name</th>
-                        <th scope="col">Middle Name</th>
-                        <th scope="col">Last Name</th>
-                        <th scope="col">Contact Number</th>
-                        <th scope="col">Email Id</th>
-                        <th scope="col">Designation</th>
-                        <th scope="col">Date Of Joining</th>
-                        <th scope="col">Actions</th>
+                    <tr class="text-wrap">
+                        <th scope="col" class="text-nowrap overflow-hidden">Id</th>
+                        <th scope="col" class="rounded-5 text-nowrap overflow-hidden">Profile Photo</th>
+                        <th scope="col" class="text-nowrap overflow-hidden">Emp ID</th>
+                        <th scope="col" class="text-nowrap overflow-hidden">First Name</th>
+                        <th scope="col" class="text-nowrap overflow-hidden">Middle Name</th>
+                        <th scope="col" class="text-nowrap overflow-hidden">Last Name</th>
+                        <th scope="col" class="text-nowrap overflow-hidden">Contact Number</th>
+                        <th scope="col" class="text-nowrap overflow-hidden">Email Id</th>
+                        <th scope="col" class="text-nowrap overflow-hidden">Designation</th>
+                        <th scope="col" class="text-nowrap overflow-hidden">Date Of Joining</th>
+                        <th scope="col" class="text-nowrap overflow-hidden">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                <?php
+                    <?php
                     while ($row = SQLSRV_FETCH_ARRAY($query, SQLSRV_FETCH_ASSOC)) {
                         $img_path = "http://localhost/Php-Admin-Dashboard/pages/Profile_photo/";
                         $date_of_joining = date('Y-m-d', strtotime($row['date_of_joining']->format('Y-m-d')));
                         $birthdate = date('Y-m-d', strtotime($row['birthdate']->format('Y-m-d')));
-                ?>
-                    <tr>
-                        <td><?php echo $row['id'] ?></td>
-                        <td><img src="<?php echo $img_path . $row['profile_image'] ?>" width="40px" height="40px"></td>
-                        <td class="emp_id"><?php echo $row['emp_id'] ?></td>
-                        <td class="first_name"><?php echo $row['first_name'] ?></td>
-                        <td class="middle_name"><?php echo $row['middle_name'] ?></td>
-                        <td class="last_name"><?php echo $row['last_name'] ?></td>
-                        <td class="contact_number"><?php echo $row['contact_number'] ?></td>
-                        <td class="email_id"><?php echo $row['email_id'] ?></td>
-                        <td class="designation"><?php echo $row['designation'] ?></td>
-                        <td class="date_of_joining"><?php echo $date_of_joining ?></td>
-                        <td>
-                            <a href="user_data_update.php?id=<?php echo $row['id']?>" class="btn btn-primary edit"><i class='bx bxs-edit'></i>&nbsp;&nbsp;Edit</a>
-                            <button type="button" class="btn btn-danger soft_delete" id="<?php echo $row['id']?>"><i class='bx bxs-trash'></i>
-                            Delete
-                            </button>
-                            <button type="button" class="btn btn-warning pdf" data-record-id="<?php echo $row['id'] ?>">
-                            <i class='bx bxs-file-pdf'></i> &nbsp;pdf
-                            </button>
-                            <!-- <button type="button" class="btn btn-warning pdf" style="" data-bs-toggle="modal" data-bs-target="#deletemodal" data-record-id="<?php echo $row['id'] ?>">
-                            <i class='bx bxs-file-pdf'></i> &nbsp;pdf
-                            </button> -->
-                        </td>
-                    </tr>
-                <?php } ?>
+                    ?>
+                        <tr>
+                            <td><?php echo $row['id'] ?></td>
+                            <td><img src="<?php echo $img_path . $row['profile_image'] ?>" width="40px" height="40px"></td>
+                            <td class="emp_id"><?php echo $row['emp_id'] ?></td>
+                            <td class="first_name"><?php echo $row['first_name'] ?></td>
+                            <td class="middle_name"><?php echo $row['middle_name'] ?></td>
+                            <td class="last_name"><?php echo $row['last_name'] ?></td>
+                            <td class="contact_number"><?php echo $row['contact_number'] ?></td>
+                            <td class="email_id"><?php echo $row['email_id'] ?></td>
+                            <td class="designation"><?php echo $row['designation'] ?></td>
+                            <td class="date_of_joining"><?php echo $date_of_joining ?></td>
+                            <td class="d-flex align-items-center gap-1">
+                                <a href="user_data_update.php?id=<?php echo $row['id'] ?>" class="btn btn-primary edit"><i class='bx bxs-edit'></i></a>
+                                <button type="button" class="btn btn-danger soft_delete" id="<?php echo $row['id'] ?>"><i class='bx bxs-trash'></i>
+                                </button>
+                                <button type="button" class="btn btn-warning pdf" data-record-id="<?php echo $row['id'] ?>">
+                                    <i class='bx bxs-file-pdf'></i>
+                                </button>
+                            </td>
+                        </tr>
+                    <?php } ?>
                 </tbody>
             </table>
         </div>
     </div>
 </body>
+
 </html>
 <script>
     $('#userPage').addClass('active');
     $(document).ready(function() {
-        $('#tableData').DataTable({
+        $('#userDataTable').DataTable({
             info: false,
             lengthChange: false,
             dom: 'Bfrtip',
             buttons: ['pageLength', 'copy', 'excel', 'pdf', 'colvis'],
             stateSave: true
         });
+        $('#userDataTable_wrapper').addClass('mb-5');
     });
+   
 
     $(document).on("click", ".soft_delete", function() {
         var restoreId = $(this).attr('id');
         // console.log(restoreId);
-        if(confirm('Are You Sure')){
+        if (confirm('Are You Sure')) {
             $.ajax({
                 url: 'user_db.php',
                 method: 'POST',
-                data: {restoreId:restoreId},
+                data: {
+                    restoreId: restoreId
+                },
                 success: function(data) {
-                alert(data);
-                 window.reload();
+                    alert(data);
+                    window.reload();
                 },
                 error: function(data) {
-                console.log(error);
+                    console.log(error);
                 }
             });
-        }else{
+        } else {
             return false;
         }
     });
 </script>
 <?php
-  include('../includes/footer.php');
+include('../includes/footer.php');
 ?>
